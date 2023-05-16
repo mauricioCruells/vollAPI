9494e2a28abc9cce5494abb9dbd5baa9e3193820
Mauricio Cruells <mcruells.dev@gmail.com>
2023-05-16 14:29:05 -0600

    :sparkles: Add pagination getMedicsPaginated

--- src/main/java/med/voll/api/controller/MedicController.java
+++ src/main/java/med/voll/api/controller/MedicController.java
@@ -1,8 +1,9 @@
 package med.voll.api.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,7 +30,7 @@ public class MedicController {
     }
 
     @GetMapping
-    public List<PaginatedMedicsDoc> getMedicsPaginated() {
-        return medicRepository.findAll().stream().map(PaginatedMedicsDoc::new).toList();
+    public Page<PaginatedMedicsDoc> getMedicsPaginated(@PageableDefault(size = 4) Pageable pageable) {
+        return medicRepository.findAll(pageable).map(PaginatedMedicsDoc::new);
     }
 }
